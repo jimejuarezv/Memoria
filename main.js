92ab8bc0d099259c1709adb47cc9251699129bd5
jimejuarezv <126489699+jimejuarezv@users.noreply.github.com>
2024-02-01 18:05:05 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//CARTAS
+let cards1 = ["🐯","🐼","🦩","🦜","🪽","🐦‍⬛","🪿","🐸","🐞","🐝","🦋","🐌"] ;
+let cards2 = ["🐯","🐼","🦩","🦜","🪽","🐦‍⬛","🪿","🐸","🐞","🐝","🦋","🐌"] ;
+let todas_las_tarjetas = cards1.concat(cards2);
+
+function repartir_cartas(){
+     
+        let tablero = document.querySelector("#root");
+    
+    todas_las_tarjetas.forEach((cada_carta)=>{
+
+    let cart= document.createElement("div");
+    cart.innerHTML =  `<div class="carta">${cada_carta}</div>` ;
+
+    tablero.appendChild(cart);
+})
+
+
+}
+
+repartir_cartas();
+
+
